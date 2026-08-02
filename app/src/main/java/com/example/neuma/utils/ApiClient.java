7cef46b1210f5ed377863b0800bad9e5135b0f8d
JuliusBourbon <naherrrrr@gmail.com>
2026-08-02 23:55:50 +0700

    url changed

--- app/src/main/java/com/example/neuma/utils/ApiClient.java
+++ app/src/main/java/com/example/neuma/utils/ApiClient.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://192.168.1.87:3000/api/";
+    private static final String BASE_URL = "https://neuma-be.vercel.app/api/";
     private static Retrofit retrofit = null;
 
     // Client Publik (Login / Register)
